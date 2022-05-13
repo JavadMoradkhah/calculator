cf1e911f23e081ff75030768e400718d00a23193
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-05-13 17:17:31 +0430

    Adding styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="calculator__display">
+        <div className="prev-operand">123 +</div>
+        <div className="curr-operand">0</div>
+      </div>
+
+      <button className="col-span-2">C</button>
+      <button>DEL</button>
+      <button>÷</button>
+
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>×</button>
+
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+
+      <button>•</button>
+      <button>0</button>
+      <button className="col-span-2 equal-btn">=</button>
     </div>
   );
 }
